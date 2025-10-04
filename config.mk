125eabf3b65392a9e564960a357d0015baf42a18
bp <bp@bpsite.xyz>
2025-10-04 13:17:25 -0700

    fix: remove optimizations from debug cflags

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ VERSION=1.1
 CFLAGS_RELEASE = -O2 -Wall -Wextra -Werror -march=native -D_FORTIFY_SOURCE=2 -fstack-protector-strong \
 				 -fstack-clash-protection -fsanitize=address
 LDFLAGS_RELEASE =
-CFLAGS_DEBUG = -O1 -Wall -march=native -g -pipe -D_FORTIFY_SOURCE=2 -fstack-protector-strong \
+CFLAGS_DEBUG = -Wall -march=native -g -pipe -D_FORTIFY_SOURCE=2 -fstack-protector-strong \
 			   -fstack-clash-protection -fno-omit-frame-pointer -fsanitize=address
 LDFLAGS_DEBUG = -fsanitize=address
 
