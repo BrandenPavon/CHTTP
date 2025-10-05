50c2007177a461de5e66db7b4bb74eebbd30fb61
bp <bp@bpsite.xyz>
2025-10-05 11:12:29 -0700

    fix: remove address sanitize from release cflags

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 VERSION=1.1
 
 CFLAGS_RELEASE = -O2 -Wall -Wextra -Werror -march=native -D_FORTIFY_SOURCE=2 -fstack-protector-strong \
-				 -fstack-clash-protection -fsanitize=address
+				 -fstack-clash-protection
 LDFLAGS_RELEASE =
 CFLAGS_DEBUG = -Wall -march=native -g -pipe -D_FORTIFY_SOURCE=2 -fstack-protector-strong \
 			   -fstack-clash-protection -fno-omit-frame-pointer -fsanitize=address
